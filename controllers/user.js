f562199de484f9d4680192fb8cfa9ed502f64581
espoirndayishimiye4 <espoirndayishimiye4@gmail.com>
2022-08-05 10:53:09 -0700

    feat: token

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,25 @@
+const { v4: uuidv4 } = require("uuid");
+const {BadRequest} = require('http-errors')
+const { createClient } = require("redis");
+const client = createClient();
+
 const User = require('../models/user')
+const {sendAccountActivationEmail} = require('../services/email')
 
 const register = async (req, res)=>{
+    const token = uuidv4()
+    const isEmailExist = await User.findOne({email:req.body.email})
+    if(isEmailExist) throw new BadRequest('user with this email Already Exist')
     const user = new User(req.body)
     const newUser = await user.save()
+    const userData = {
+        id: user._id,
+        email: user.email,
+      };
+      await client.connect();
+      await client.hSet(token, userData);
+      await client.disconnect()
+    await sendAccountActivationEmail(token, user)
     res.status(201).json({
         success: true,
         data: newUser
--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const RedisStore = require("connect-redis")(session)
 require('dotenv').config()
 const dbConn = require('./config/dbConn')
 const app = express()
 
+//connect to redis
+const { createClient } = require("redis")
+let redisClient = createClient({ legacyMode: true })
+redisClient.connect().catch(console.error)
+
+//use session and redis
+app.use(
+    session({
+      store: new RedisStore({ client: redisClient }),
+      saveUninitialized: false,
+      secret: "keyboard cat",
+      resave: false,
+    })
+  )
+
 dbConn()
 
+
+
 app.use(express.json())
+
+
+
 app.use('/user', require('./routes/user'))
 
 mongoose.connection.once('open', ()=>{
